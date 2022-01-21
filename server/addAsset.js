0229b076e1c06976fb9e44e117428e2c9c79231e
Adam Gibbs <adam-gibbs@outlook.com>
2022-01-21 03:46:39 +0000

    Add userId index to asset

--- server/addAsset.js
+++ server/addAsset.js
@@ -6,7 +6,7 @@ const db = require('./dynamodb');
 module.exports.addAsset = (event, context, callback) => {
   const timestamp = new Date().getTime();
   const data = JSON.parse(event.body);
-  if (typeof data.co2 !== 'number' || typeof data.description !== 'string' || typeof data.eol !== 'string' || typeof data.image !== 'string'|| typeof data.location !== 'string'|| typeof data.name !== 'string'|| typeof data.type !== 'string') {
+  if (typeof data.userId !== 'string' || typeof data.co2 !== 'number' || typeof data.description !== 'string' || typeof data.eol !== 'string' || typeof data.image !== 'string'|| typeof data.location !== 'string'|| typeof data.name !== 'string'|| typeof data.type !== 'string') {
     console.error('Validation Failed');
     callback(null, {
       statusCode: 400,
@@ -23,6 +23,7 @@ module.exports.addAsset = (event, context, callback) => {
     TableName: process.env.ASSETS_TABLE,
     Item: {
       id: uuid.v1(),
+      userId: data.userId,
       c02: data.co2,
       description: data.description,
       eol: data.eol,
