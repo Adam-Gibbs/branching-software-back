e55805dd33adcf69e78ec381f744c93336d58e95
Adam Gibbs <adam-gibbs@outlook.com>
2022-01-21 05:00:10 +0000

    Add all asset get

---
+++ server/allAssets.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const db = require('./dynamodb');
+
+module.exports.allAssets = (event, context, callback) => {
+  const data = JSON.parse(event.body);
+  if (typeof data.userId !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: {    
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({message: 'Invalid data'}),
+    });
+    return;
+  }
+
+  const params = {
+    TableName: process.env.ASSETS_TABLE,
+    IndexName: 'userId-asset-index',
+    KeyConditionExpression: 'userId = :u',
+    ExpressionAttributeValues: {
+    ':u': data.userId
+    }
+  };
+
+  // fetch todo from the database
+  db.query(params, (error, result) => {
+    let response = {
+      statusCode: 401,
+      headers: {    
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({message: 'You do not have any assets'}),
+    };
+
+    if (error) {
+      console.error(error);
+      callback(null, response);
+      return;
+    }
+    
+    try {
+      if (result.Items) {
+        response = {
+          statusCode: 201,
+          headers: {    
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+          },
+          body: JSON.stringify({message: 'Success', ...result.Items}),
+        };
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      callback(null, response);
+    }
+  });
+};
