4d9d6ff5432cc495e38e327046ad9c870e6fb202
Adam Gibbs <adam-gibbs@outlook.com>
2022-01-22 19:29:31 +0000

    All locations endpoint

---
+++ server/v1/locations/all.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const db = require('../../dynamodb');
+
+module.exports.all = (event, context, callback) => {
+  const data = JSON.parse(event.body);
+  if (typeof data.userId !== 'string') {
+    console.log('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: {    
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({message: 'Invalid data'}),
+    });
+    return;
+  }
+
+  const params = {
+    TableName: process.env.LOCATIONS_TABLE,
+    IndexName: 'userId-location-index',
+    KeyConditionExpression: 'userId = :u',
+    ExpressionAttributeValues: {
+    ':u': data.userId
+    }
+  };
+
+  // get all locations from the database
+  db.query(params, (error, result) => {
+    let response = {
+      statusCode: 401,
+      headers: {    
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({message: 'An error occurred while retrieving locations'}),
+    };
+
+    if (error) {
+      console.log(error);
+      callback(null, response);
+      return;
+    }
+    
+    try {
+      if (result.Items) {
+        response = {
+          statusCode: 201,
+          headers: {    
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+          },
+          body: JSON.stringify({message: 'Success', result: result.Items}),
+        };
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      callback(null, response);
+    }
+  });
+};
