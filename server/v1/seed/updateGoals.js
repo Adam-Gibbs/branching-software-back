cf91cb44510765f9c81681e4f2e8474b86a3eaa9
Adam Gibbs <adam-gibbs@outlook.com>
2022-01-23 23:17:10 +0000

    Seed goals

---
+++ server/v1/seed/updateGoals.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const sr = require('../../helper/SendResponse');
+const db = require('../../helper/db/Add');
+const faker = require('@ngneat/falso');
+
+module.exports.addGoal = (event, context, callback)  =>  {
+  const data = JSON.parse(event.body);
+  if (vr.validateRequest(data, [{name: 'userId', type: 'string'}, {name: 'number', type: 'number'}], sr.sendResponse, callback)) {
+    add(4, data.userId, callback);
+  }
+};
+
+function add(value, userId, callback) {
+  var types = [
+    'Assets Nearing EOL',
+    'Average Time Between Actions',
+    'Days Since Last Action',
+    'Difference to Target Estimate',
+    'Number of Ongoing Projects',
+    'Possible Daily Savings',
+    'Registered Assets',
+    'Total Assets Replaced',
+    'Total Carbon Emissions',
+  ];
+
+  const params = {
+    userId: userId,
+    name: faker.randQuote(),
+    type: types[Math.floor(Math.random() * types.length)],
+    targetValue: faker.randNumber({ min: 10, max: 1000 }),
+  };
+
+  db.add(process.env.GOALS_TABLE, params, complete, {value: value, userId: userId, callback: callback});
+}
+
+function complete(data, carry) {
+  if (data.statusCode === 201) {
+    if (carry.value === 0) {
+      sr.sendResponse({statusCode: 201, return: {message: 'Success', result: params.Item}}, carry.callback);
+    } else {
+      add(carry.value - 1, carry.userId, carry.callback);
+    }
+  } else {
+    sr.sendResponse(
+      {
+        statusCode: error.statusCode || 501,
+        return: {message: `An error occurred, please try again`}
+      },
+      callback
+    );
+  }
+}
