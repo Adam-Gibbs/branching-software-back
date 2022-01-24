83d5a272790495741c4a5ffaf6b72741f12dafe2
Adam Gibbs <adam-gibbs@outlook.com>
2022-01-24 00:06:32 +0000

    Add handler for asset update

--- server/v1/seed/updateAssets.js
+++ server/v1/seed/updateAssets.js
@@ -5,7 +5,7 @@ const sr = require('../../helper/SendResponse');
 const db = require('../../helper/db/Add');
 const faker = require('@ngneat/falso');
 
-module.exports.updateGoals = (event, context, callback)  =>  {
+module.exports.updateAssets = (event, context, callback)  =>  {
   const data = JSON.parse(event.body);
   if (vr.validateRequest(data, [{name: 'userId', type: 'string'}, {name: 'number', type: 'number'}], sr.sendResponse, callback)) {
     add(data.number, data.userId, callback);
